faa1caedcb613e0591976b638b4f506b538cfd26
Cristian Leiva <cleiva94.cl@gmail.com>
2019-04-13 16:35:00 -0400

    signup/access users fetch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,47 @@ import AnxietyForm from './AnxietyForm'
 import HomePage from './HomePage'
 
 class App extends Component {
+
+  state = {  
+    user:{}
+  }
+
+  componentDidMount = () => {
+    if (localStorage.getItem('token')) {
+      fetch('http://localhost:3000/current_user', {
+        headers: {
+          'Content-Type': 'application/json',
+          'accept': 'application/json',
+          'Authorization': localStorage.getItem('token')
+        }
+      })
+      .then( res => res.json())
+      .then(userJson => {
+        this.setState({
+          user: userJson.user
+        }, () => console.log(this.state))
+      })
+    }
+  }
+
+  sendSignUp = userObj => {
+    fetch('http://localhost:3000/current_user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'accepts': 'application/json'
+      },
+      body: JSON.stringify({user: userObj})
+    })
+    .then(res => res.json())
+    .then(userData => {
+      this.setState({
+        user: userData.user
+      }, () => {
+        localStorage.setItem("token", userData.jwt);
+      })
+    })
+  }
   render() {
     return (
       <div className="App">
