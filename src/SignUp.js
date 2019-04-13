8c00fa7a107963fc2856170e103b92a319764a52
github username <github email address>
2019-04-13 14:40:35 -0400

    basic sign up and log in forms

---
+++ src/SignUp.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class SignUp extends React.Component {
+  state = {
+    name: '',
+    password: ''
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    }, () => console.log(this.state))
+  }
+
+  render(){
+    return (
+      <div>
+        <form onSubmit={console.log}>
+          <input type="text" name="name" placeholder="Username" onChange={this.changeHandler} value={this.state.name}/>
+          <input type="password" name="password" placeholder="Password" onChange={this.changeHandler} value={this.state.password}/>
+          <input type="submit" value="Sign Up"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignUp;
