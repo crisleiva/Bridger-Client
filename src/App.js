e4597106d006975ba242e95b4255e02f9360b6d5
MichaelRomero <liikemike@aim.com>
2019-04-13 18:10:33 -0400

    fixed merge on App

--- src/App.js
+++ src/App.js
@@ -10,6 +10,56 @@ import {Route, Switch, Link, withRouter} from 'react-router-dom'
 
 
 class App extends Component {
+  state ={
+    user: {}
+  }
+
+  componentDidMount = () => {
+    if (localStorage.getItem('token')) {
+      fetch('http://localhost:3001/current_user', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': localStorage.getItem('token')
+        }
+      })
+      .then( res => res.json())
+      .then(userJson => {
+        this.setState({
+          user: userJson.user
+        }, () => console.log(this.state))
+      })
+    }
+  }
+
+  sendSignUp = (userObj,e) => {
+    e.preventDefault()
+    fetch('http://localhost:3001/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({user: {...userObj, email: 'anythng' }  })
+    })
+    .then(res => res.json())
+    .then(userData => {
+      this.setState({
+        user: userData.user
+      }, () => {
+        localStorage.setItem("token", userData.jwt);
+      })
+    })
+  }
+
+  handleLogOut = () => {
+    this.setState({
+      user: {}
+    })
+    localStorage.removeItem('token')
+
+  }
+  
   render() {
     return (
       <div className="App">
