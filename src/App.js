9b84dc5c7fad7ad6cf14f7709e483604c658cdf0
github username <github email address>
2019-04-13 15:08:27 -0400

    basic forms built

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SignUp from './SignUp'
 import LogIn from './LogIn'
+import AnxietyForm from './AnxietyForm'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <SignUp />
         <LogIn />
+        <AnxietyForm />
       </div>
     );
   }
