e5a7ffc71dfa06fc78faa853213938aacb7bc24b
MichaelRomero <liikemike@aim.com>
2019-04-13 17:37:37 -0400

    added browser router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LogIn from './LogIn'
 import AnxietyForm from './AnxietyForm'
 import HomePage from './HomePage'
 import DashBoard from './Dashboard'
+import {Route, Switch, Link} from 'react-router-dom'
 
 
 
@@ -12,9 +13,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
-        <SignUp />
-        <LogIn />
+        <Switch>
+          <Route path="/login" component={LogIn}/>
+          <Route path="/signup" component={SignUp}/>
+          <Route path="/" component={HomePage}/>
+        </Switch>
       </div>
     );
   }
