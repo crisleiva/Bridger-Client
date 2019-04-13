76e28e356d5e6b1c22c0e3e41cce98f2e17a8576
github username <github email address>
2019-04-13 17:10:14 -0400

    pre-merge

--- src/App.js
+++ src/App.js
@@ -4,56 +4,16 @@ import SignUp from './SignUp'
 import LogIn from './LogIn'
 import AnxietyForm from './AnxietyForm'
 import HomePage from './HomePage'
+import DashBoard from './Dashboard'
+
 
 class App extends Component {
-
-  state = {  
-    user:{}
-  }
-
-  componentDidMount = () => {
-    if (localStorage.getItem('token')) {
-      fetch('http://localhost:3000/current_user', {
-        headers: {
-          'Content-Type': 'application/json',
-          'accept': 'application/json',
-          'Authorization': localStorage.getItem('token')
-        }
-      })
-      .then( res => res.json())
-      .then(userJson => {
-        this.setState({
-          user: userJson.user
-        }, () => console.log(this.state))
-      })
-    }
-  }
-
-  sendSignUp = userObj => {
-    fetch('http://localhost:3000/current_user', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'accepts': 'application/json'
-      },
-      body: JSON.stringify({user: userObj})
-    })
-    .then(res => res.json())
-    .then(userData => {
-      this.setState({
-        user: userData.user
-      }, () => {
-        localStorage.setItem("token", userData.jwt);
-      })
-    })
-  }
   render() {
     return (
       <div className="App">
         <HomePage />
         <SignUp />
         <LogIn />
-        <AnxietyForm />
       </div>
     );
   }
---
+++ src/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AnxietyForm from './AnxietyForm'
+import DailyGraph from './DailyGraph'
+
+const DashBoard = () => {
+  return (
+    <div>
+      <AnxietyForm />
+      <DailyGraph />
+    </div>
+  )
+
+}
+
+export default DashBoard;
