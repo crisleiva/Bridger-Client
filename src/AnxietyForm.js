9bb01442a3bc50695331865d3bafe584235dc235
MichaelRomero <liikemike@aim.com>
2019-04-13 19:21:30 -0400

    fixed the anxiety form

--- src/AnxietyForm.js
+++ src/AnxietyForm.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class AnxietyForm extends React.Component {
   state = {
-    anxietyType: '',
+    content: '',
     anxietyRating: '',
     sleep: '',
     diet: '',
@@ -15,28 +15,31 @@ dailyChangeHandler = (e) => {
   })
 }
 
-handleSubmit = (obj, e) => {
+handleSubmit = (e, obj, user) => {
+  const newEvent = {...obj, user_id: user.id}
   e.preventDefault()
+
   fetch('http://localhost:3001/events', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json'
     },
-    body: JSON.stringify({
-      events: obj
+    body: JSON.stringify({event: newEvent})
     })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+    debugger
+    // CONNECT TO GRAPH HERE!
   })
 }
 
   render(){
     return (
       <div>
-        <form onSubmit={(e) => this.handleSubmit(this.state, e)}>
+        <form onSubmit={(e) => this.handleSubmit(e, this.state, this.props.user)}>
         <h3>What was your biggest cause of anxiety today?</h3>
-        <input type="text" name="anxietyType" placeholder="Anxiety" onChange={this.dailyChangeHandler} value={this.state.anxietyType}/>
+        <input type="text" name="content" placeholder="Anxiety" onChange={this.dailyChangeHandler} value={this.state.anxietyType}/>
         <h3>On a scale of 1-10 how bad was it today?</h3>
         <input type="text" name="anxietyRating" placeholder="1-10" onChange={this.dailyChangeHandler} value={this.state.anxietyRating}/>
         <h3>On a scale of 1-10 how content are you with the amount of sleep you got last night?</h3>
